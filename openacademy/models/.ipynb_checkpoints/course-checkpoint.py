c934d49e1b11e66801429e7d289284743407dc9c
mis2000 <gross.israel@gmail.com>
2020-02-04 19:21:46 +0000

    Added Firlds

--- openacademy/models/.ipynb_checkpoints/course-checkpoint.py
+++ openacademy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -1,6 +1,12 @@
 # -*- coding: utf8 -*-
-from odoo import models
+from odoo import models, fields
 
 class OpenAcademyCours(models.Model):
     _name = 'openacademy.course'
     _description = 'OpenAcademy Course'
+    
+    name = fields.char(string='Course Tilte', required=True,
+                      index=True, help='Enter your course title on this field,')
+    description = fields.html(string'Description')
+    banner = fields.Binary(string='Banner')
+    price = fields.float(string='Price' Digits (5,4))
