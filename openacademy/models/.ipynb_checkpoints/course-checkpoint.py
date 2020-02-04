34037df67178c1f426856e2c6fcecc0bf8ea7acf
mis2000 <gross.israel@gmail.com>
2020-02-04 20:16:44 +0000

    Added Relational Fields

--- openacademy/models/.ipynb_checkpoints/course-checkpoint.py
+++ openacademy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -4,6 +4,14 @@ from odoo import models, fields
 class OpenAcademyCours(models.Model):
     _name = 'openacademy.course'
     _description = 'OpenAcademy Course'
+
+    
+class OpenAcademyTags(models.Model):
+    _name = 'openacademy.tags'
+    _description = 'Openacademy Tags'
+    name = fields.char(string='Name')
+    
+    
     
     name = fields.char(string='Course Tilte', required=True,
                       index=True, help='Enter your course title on this field,')
@@ -12,3 +20,8 @@ class OpenAcademyCours(models.Model):
     price = fields.float(string='Price' Digits (5,4))
     expire_date = fields.date(string='Expire After', required=True)
     
+    responsible_id = fields.Many2one(comodel_name='res.users', required=True, string='Responsible',
+                                   ondelete='restrict', copy='False')
+    
+    tag_ids = fields.Many2Many(comodel_name='openacademy.tags', relation='rel_course_tags',
+                              column1='course_id', column2='Tag_Id', string='Tags')
